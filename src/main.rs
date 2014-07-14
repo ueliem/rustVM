c813ba6308f73b6ba7b18c16afecd4bbc801e37b
Will Long <will.long043@gmail.com>
2014-07-14 08:29:16 -0400

    Added functionality for subroutines.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,25 @@ use std::io;
 mod vm;
 
 fn main() {
-    let const_list: &[vm::Constant<u16>] = [vm::IntConstant(1_u16)];
+    let const_list: &[vm::Constant<u16>] = [vm::IntConstant(1_u16), vm::IntConstant(2_u16), vm::IntConstant(3_u16)];
 
     let instr_list: &[vm::Bytecode] = [
-    vm::Bytecode{opcode: vm::ILoadConst, firstbyte: Some(0_u8), secondbyte: Some(0_u8)},
-    vm::Bytecode{opcode: vm::ILoadConst, firstbyte: Some(0_u8), secondbyte: Some(0_u8)},
-    vm::Bytecode{opcode: vm::ISum, firstbyte: None, secondbyte: None},
-    vm::Bytecode{opcode: vm::IReturn, firstbyte: None, secondbyte: None}
+    vm::Bytecode{opcode: vm::Goto, firstbyte: Some(0_u8), secondbyte: Some(6_u8)},//Go to first real instruction of program
+    //Subroutine
+    vm::Bytecode{opcode: vm::ILoadConst, firstbyte: Some(0_u8), secondbyte: Some(1_u8)},//Load 2
+    vm::Bytecode{opcode: vm::ILoadConst, firstbyte: Some(0_u8), secondbyte: Some(1_u8)},//Load 2
+    vm::Bytecode{opcode: vm::ISum, firstbyte: None, secondbyte: None},//Sum 2 + 2
+    vm::Bytecode{opcode: vm::Print, firstbyte: None, secondbyte: None},//Print 4
+    vm::Bytecode{opcode: vm::IReturn, firstbyte: None, secondbyte: None},
+    //End Subroutine
+    //Begin Program
+    vm::Bytecode{opcode: vm::ILoadConst, firstbyte: Some(0_u8), secondbyte: Some(1_u8)},//Load 2
+    vm::Bytecode{opcode: vm::ILoadConst, firstbyte: Some(0_u8), secondbyte: Some(2_u8)},//Load 3
+    vm::Bytecode{opcode: vm::ISum, firstbyte: None, secondbyte: None},//Sum 2 + 3
+    vm::Bytecode{opcode: vm::Print, firstbyte: None, secondbyte: None},//Print 5
+    vm::Bytecode{opcode: vm::Call, firstbyte: Some(0_u8), secondbyte: Some(1_u8)},//,//Call subroutine starting at index 1
+    vm::Bytecode{opcode: vm::Print, firstbyte: None, secondbyte: None}//Prints 4 (Returned value from subroutine)
+    //End Program
     ];
 
     vm::execute_vm(true, instr_list, const_list);
--- src/vm.rs
+++ src/vm.rs
@@ -4,15 +4,18 @@ enum StackItem<u16, String> {
 }
 
 pub fn execute_vm(strict: bool, bytecodes: &[Bytecode], constant_pool: &[Constant<u16>]) {
-    let mut stack: Vec<StackItem<int, &str>> = Vec::new();
+    let mut stack: Vec<StackItem<u16, String>> = Vec::new();
+    let mut frame_pointers: Vec<uint> = Vec::new();
     let mut instruction_pointer = 0;
-    loop {
+    while instruction_pointer <  bytecodes.len() {
+        // println!("{}", stack);
         match bytecodes[instruction_pointer].opcode {
-            Nop => continue,
+            Nop => instruction_pointer += 1,
             ISum => {
                 match (stack.pop(), stack.pop()) {
                     (Some(StackInteger(tos)),Some(StackInteger(tos1))) => {
                         stack.push(StackInteger(tos + tos1));
+                        instruction_pointer += 1;
                     },
                     _ => fail!()
                 }
@@ -21,27 +24,79 @@ pub fn execute_vm(strict: bool, bytecodes: &[Bytecode], constant_pool: &[Constan
                 match (stack.pop(), stack.pop()) {
                     (Some(StackInteger(tos)),Some(StackInteger(tos1))) => {
                         stack.push(StackInteger(tos - tos1));
+                        instruction_pointer += 1;
                     },
                     _ => fail!()
                 }
             },
             ILoadConst => {
                 match constant_pool[((bytecodes[instruction_pointer].firstbyte.unwrap() << 8) as uint) + (bytecodes[instruction_pointer].secondbyte.unwrap() as uint)] {
-                    IntConstant(n) => stack.push(StackInteger(n))//,
+                    IntConstant(n) => {
+                        stack.push(StackInteger(n));
+                        instruction_pointer += 1;
+                    }
                 }
             },
+            // IReturn => {
+            //     match stack.pop() {
+            //         Some(StackInteger(tos)) => {
+            //             println!("{}", tos);
+            //             // instruction_pointer += 1;
+            //             return;
+            //         },
+            //         _ => fail!()
+            //     }
+            // },
             IReturn => {
                 match stack.pop() {
                     Some(StackInteger(tos)) => {
+                        match frame_pointers.pop() {
+                            Some(stack_address) => {
+                                //Pop everything in the current frame until the ret_addr off the stack
+                                while stack_address < stack.len() - 1 {
+                                    stack.pop();
+                                    // println!("{}, {}", stack_address, stack.len());
+                                }
+                                //grab the ret_addr
+                                let probable_return_address = stack.pop();
+                                match probable_return_address {
+                                    Some(StackInteger(return_address)) => {
+                                        instruction_pointer = return_address as uint;
+                                    },
+                                    _ => fail!()
+                                };
+
+                                //push tos
+                                stack.push(StackInteger(tos));
+                            },
+                            None => fail!()
+                        }
+                    },
+                    _ => fail!()
+                }
+            },
+            Goto => {
+                instruction_pointer = ((bytecodes[instruction_pointer].firstbyte.unwrap() << 8) as uint) + (bytecodes[instruction_pointer].secondbyte.unwrap() as uint);
+            },
+            Call => {
+                stack.push(StackInteger(instruction_pointer as u16 + 1));
+                frame_pointers.push(stack.len() - 1);
+                instruction_pointer = ((bytecodes[instruction_pointer].firstbyte.unwrap() << 8) as uint) + (bytecodes[instruction_pointer].secondbyte.unwrap() as uint);
+            },
+            Print => {
+                match stack.pop() {
+                    Some(StackInteger(tos)) => {
                         println!("{}", tos);
-                        return;
+                        stack.push(StackInteger(tos));
+                        instruction_pointer += 1
                     },
                     _ => fail!()
                 }
             }
         }
-        instruction_pointer += 1;
+        // println!("{}, {}", instruction_pointer, bytecodes.len());
     }
+    // println!("Done");
 }
 
 pub struct Bytecode {
@@ -56,6 +111,9 @@ pub enum Opcode {
   ISub = 0x02,
   IReturn = 0x03,
   ILoadConst = 0x04,
+  Goto = 0x05,
+  Call = 0x06,
+  Print = 0x07
 }
 
 pub enum Constant<u16> {
